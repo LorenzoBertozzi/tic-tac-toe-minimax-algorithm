b09878bef0fa957050163cfa3ce84194260ed76d
Xoriz <37419347+ReiDaBatata@users.noreply.github.com>
2023-09-20 10:25:20 -0300

    testing code train

--- minimax.js
+++ minimax.js
@@ -1,4 +1,4 @@
-//Integraçao de minimax para analise de jogada
+/*//Integraçao de minimax para analise de jogada
 function bestMove(){
     //jogada da IA
     let bestScore = -Infinity;
@@ -61,4 +61,71 @@ function minimax(board, depth, isMaximizing){
         }
         return bestScore;
     }
-}
+}*/
+
+//code train code
+
+function bestMove() {
+    // AI to make its turn
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = ai;
+          let score = minimax(board, 0, false);
+          board[i][j] = '';
+          if (score > bestScore) {
+            bestScore = score;
+            move = { i, j };
+          }
+        }
+      }
+    }
+    board[move.i][move.j] = ai;
+    currentPlayer = human;
+  }
+  
+  let scores = {
+    X: 10,
+    O: -10,
+    tie: 0
+  };
+  
+  function minimax(board, depth, isMaximizing) {
+    let result = checkWinner();
+    if (result !== null) {
+      return scores[result];
+    }
+  
+    if (isMaximizing) {
+      let bestScore = -Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = ai;
+            let score = minimax(board, depth + 1, false);
+            board[i][j] = '';
+            bestScore = max(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = Infinity;
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          // Is the spot available?
+          if (board[i][j] == '') {
+            board[i][j] = human;
+            let score = minimax(board, depth + 1, true);
+            board[i][j] = '';
+            bestScore = min(score, bestScore);
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
