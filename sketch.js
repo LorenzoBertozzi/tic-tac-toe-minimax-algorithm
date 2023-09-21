26ccf9846e14dfbb22b13f894ce006db0c2e01eb
Xoriz <37419347+ReiDaBatata@users.noreply.github.com>
2023-09-21 08:22:51 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,3 @@
-/*
 //criando matrix tabuleiro
 let board = [
     ['','',''],
@@ -6,8 +5,8 @@ let board = [
     ['','','']
 ];
 
-let w; //largura = 3;
-let h; //altura = 3;
+let w; //largura / 3;
+let h; //altura / 3;
 
 //declarando jogadores;
 let ai = 'X';
@@ -16,14 +15,14 @@ let currentPlayer = human; //jogador atual
 
 //plotando tabuleiro
 function setup(){
-    createCanvas(400,400);
+    createCanvas(800,800);
     w = width / 3;
-    h = heigth / 3;
-    bestMove();
+    h = height / 3;
+    //bestMove();  //comente essa linha para o primiero jogador ser o human
 }
 
 function equals3(a,b,c){
-    return a == b && b == c && a!= '';
+    return a == b && b == c && a != '';
 }
 
 function checkWinner(){
@@ -37,18 +36,18 @@ function checkWinner(){
     }
 
     //vertical 
-    for(let i= 0; i < 3; i++){
+    for(let i = 0; i < 3; i++){
         if(equals3(board[0][i],board[1][i],board[2][i])){
-            winner = board[0][1];
+            winner = board[0][i];
         }
     }
 
     //diagonal
-    if(equals3(board[2][2],board[1][1],board[0][0])){
-        winner = board[2][2];
+    if (equals3(board[0][0], board[1][1], board[2][2])) {
+      winner = board[0][0];
     }
-    if(equals3(board[2][0],board[1][1],board[0][2])){
-        winner = board[0][0];
+    if (equals3(board[2][0], board[1][1], board[0][2])) {
+      winner = board[2][0];
     }
 
     //contar espaços vazios -- identificar velha
@@ -63,11 +62,10 @@ function checkWinner(){
 
     //velha
     if(winner == null && openSpots == 0){
-        return 'tie';
+        return 'velha';
     }else{
         return winner;
     }
-
 }
 
 //clicar para jogar
@@ -76,12 +74,12 @@ function mousePressed(){
     if(currentPlayer == human){
         let i = floor(mouseX / w);
         let j = floor(mouseY / h);
-    }
-    //verificando jogada
-    if(board[i][j] == ''){
-        board[i][j] = human;
-        currentPlayer = ai;
-        bestMove();
+        //verificando jogada
+        if(board[i][j] == ''){
+            board[i][j] = human;
+            currentPlayer = ai;
+            bestMove();
+        }   
     }
 }
 
@@ -89,22 +87,23 @@ function draw(){
     background(255);
     strokeWeight(4);
 
-    line(w, 0, w, heigth);
-    line(w * 2, 0, w * 2, heigth);
+    line(w, 0, w, height);
+    line(w * 2, 0, w * 2, height);
     line(0, h, width, h);
     line(0, h * 2, width, h * 2);
 
     for(let j = 0; j < 3 ; j++){
         for(let i = 0; i < 3; i++){
-            let x = w * i + w / 2 ;
-            let y = h * j + h / 2 ;
+            let x = w * i + w / 2;
+            let y = h * j + h / 2;
             let spot = board[i][j];
             
             textSize(32);
+
             let r = w / 4;
             if(spot == human){
                 noFill();
-                ellipse(x,y,r * 2);
+                ellipse(x, y, r * 2);
             }else if(spot == ai){
                 line(x - r, y - r, x + r, y + r);
                 line(x + r, y - r, x - r, y + r);
@@ -112,136 +111,16 @@ function draw(){
         }
     }
 
+
     let result = checkWinner();
-    if(result != null){
+    if (result != null) {
         noLoop();
         let resultP = createP('');
         resultP.style('font-size', '32pt');
-        if(result == 'tie'){
-            resultP.html('Tie !!');
-        }else{
-            resultP.html('${result} wins !!');
+        if (result == 'velha') {
+            resultP.html('Velha!!');
+        } else {
+            resultP.html(`${result} Ganhou!!`);
         }
     }
 }
-*/
-
-// code-train code
-
-let board = [
-    ['', '', ''],
-    ['', '', ''],
-    ['', '', '']
-  ];
-  
-  let w; // = width / 3;
-  let h; // = height / 3;
-  
-  let ai = 'X';
-  let human = 'O';
-  let currentPlayer = human;
-  
-  function setup() {
-    createCanvas(400, 400);
-    w = width / 3;
-    h = height / 3;
-    bestMove();
-  }
-  
-  function equals3(a, b, c) {
-    return a == b && b == c && a != '';
-  }
-  
-  function checkWinner() {
-    let winner = null;
-  
-    // horizontal
-    for (let i = 0; i < 3; i++) {
-      if (equals3(board[i][0], board[i][1], board[i][2])) {
-        winner = board[i][0];
-      }
-    }
-  
-    // Vertical
-    for (let i = 0; i < 3; i++) {
-      if (equals3(board[0][i], board[1][i], board[2][i])) {
-        winner = board[0][i];
-      }
-    }
-  
-    // Diagonal
-    if (equals3(board[0][0], board[1][1], board[2][2])) {
-      winner = board[0][0];
-    }
-    if (equals3(board[2][0], board[1][1], board[0][2])) {
-      winner = board[2][0];
-    }
-  
-    let openSpots = 0;
-    for (let i = 0; i < 3; i++) {
-      for (let j = 0; j < 3; j++) {
-        if (board[i][j] == '') {
-          openSpots++;
-        }
-      }
-    }
-  
-    if (winner == null && openSpots == 0) {
-      return 'tie';
-    } else {
-      return winner;
-    }
-  }
-  
-  function mousePressed() {
-    if (currentPlayer == human) {
-      // Human make turn
-      let i = floor(mouseX / w);
-      let j = floor(mouseY / h);
-      // If valid turn
-      if (board[i][j] == '') {
-        board[i][j] = human;
-        currentPlayer = ai;
-        bestMove();
-      }
-    }
-  }
-  
-  function draw() {
-    background(255);
-    strokeWeight(4);
-  
-    line(w, 0, w, height);
-    line(w * 2, 0, w * 2, height);
-    line(0, h, width, h);
-    line(0, h * 2, width, h * 2);
-  
-    for (let j = 0; j < 3; j++) {
-      for (let i = 0; i < 3; i++) {
-        let x = w * i + w / 2;
-        let y = h * j + h / 2;
-        let spot = board[i][j];
-        textSize(32);
-        let r = w / 4;
-        if (spot == human) {
-          noFill();
-          ellipse(x, y, r * 2);
-        } else if (spot == ai) {
-          line(x - r, y - r, x + r, y + r);
-          line(x + r, y - r, x - r, y + r);
-        }
-      }
-    }
-  
-    let result = checkWinner();
-    if (result != null) {
-      noLoop();
-      let resultP = createP('');
-      resultP.style('font-size', '32pt');
-      if (result == 'tie') {
-        resultP.html('Tie!');
-      } else {
-        resultP.html(`${result} wins!`);
-      }
-    }
-  }
